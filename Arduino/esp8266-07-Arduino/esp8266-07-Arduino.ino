0d4c830fdf2c2d2e120b15040fc9c69f2a26cff6
Ariel Verdejo <verdejo.ariel@gmail.com>
2019-02-26 17:14:32 -0300

    Arreglo detalles en el codigo.

--- Arduino/esp8266-07-Arduino/esp8266-07-Arduino.ino
+++ Arduino/esp8266-07-Arduino/esp8266-07-Arduino.ino
@@ -1,18 +1,19 @@
 int i = 0;
 int j = 0;
-char c[100];
-
+char c[256];
+//int baudRate = 115200;
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
-  Serial1.setTimeout(1000);
-
+//  Serial1.setTimeout(1000);
   while(!Serial); 
   while(!Serial1);
-  
+  receiveFromESP8266();
+  delay(1000);
   sendToESP8266("AT");
   receiveFromESP8266();
-  printInSerialPort();
+//  printInSerialPort();
+  delay (5000);
 }
 
 void loop() {
@@ -20,32 +21,49 @@ void loop() {
   Serial.println();
   sendToESP8266("AT+GMR");
   receiveFromESP8266();
-  printInSerialPort();
-  
+//  printInSerialPort();
+  delay (5000);
   Serial.println();
   Serial.println();
   sendToESP8266("AT+RST");
+  delay(4);
   receiveFromESP8266();
-  printInSerialPort();
-    
+//  printInSerialPort();
+  delay (5000);  
   Serial.println();
   Serial.println();
   sendToESP8266("AT+CWLAP");
   receiveFromESP8266();
-  printInSerialPort();
+//  printInSerialPort();
+  delay (5000);
 }
 
 void sendToESP8266 (String message){
   Serial1.println(message);
-  delay (3000);
+//  Serial1.flush();
+//  delayMicroseconds(800);
+}
+
+void cleanBuffer(){
+  char c;
+  delay(5);
+  while (Serial1.available() != 0){
+    c = Serial1.read();
+  }
 }
 
 void receiveFromESP8266(){
-  i = 0;
-  while (Serial1.available() > 0) {
-    c[i] = Serial1.read();
+  char c;
+  while (Serial1.available() != 0) {
+    c = Serial1.read();
+    Serial.print(c);
+//    delayMicroseconds(50);
     i++;
-  }  
+  } 
+  Serial.println();
+  Serial.print("Datos Leidos = ");
+  Serial.println(i);
+  i=0; 
 }
 
 void printInSerialPort(){
